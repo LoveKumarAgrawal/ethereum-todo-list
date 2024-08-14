37f74b68f7b690886e6e5262c5c7a6e5f05d5362
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-08-14 17:31:03 +0530

    changed vanilla javscript to nextjs

---
+++ backend/contracts/TaskContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract TaskContract {
+  event AddTask(address recipient, uint taskId);
+  event DeleteTask(uint taskId, bool isDeleted);
+
+  struct Task {
+    uint id;
+    string taskText;
+    bool isDeleted;
+  }
+}
